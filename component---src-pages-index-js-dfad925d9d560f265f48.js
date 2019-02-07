e8b6df812a683fc2a4e1791d2b07e9f8be5d5a60
ph1p <phil@capshake.com>
2019-02-08 00:24:30 +0100

    Updates

--- component---src-pages-index-js-dfad925d9d560f265f48.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{134:function(n,e,i){"use strict";i.r(e);var t=i(0),u=i.n(t);e.default=function(){return u.a.createElement("div",null,"Hi I'm Phil!")}}}]);
-//# sourceMappingURL=component---src-pages-index-js-dfad925d9d560f265f48.js.map
